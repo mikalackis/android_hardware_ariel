487b7eca4029757a17a644d4713afc6dd29db180
Sava Mikalacki <mikalackis@gmail.com>
2017-07-13 23:02:36 +0200

    Print out some info

--- Android.mk
+++ Android.mk
@@ -1,5 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(BOARD_USES_ARIEL_HARDWARE),true)
+    $(info ABRAKADABRAAAAAAAA)
+    $(info $(BOARD_USES_ARIEL_HARDWARE))
     include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
