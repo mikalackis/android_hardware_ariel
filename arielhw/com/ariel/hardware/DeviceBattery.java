81274d2c5994577f57a8e2b192a4447c175fe8c2
Sava Mikalacki <mikalackis@gmail.com>
2017-09-17 18:44:53 +0200

    Remove awake time, add screen on time

--- arielhw/com/ariel/hardware/DeviceBattery.java
+++ arielhw/com/ariel/hardware/DeviceBattery.java
@@ -30,6 +30,18 @@ import java.security.MessageDigest;
 
 import com.android.server.am.BatteryStatsService;
 import com.android.internal.app.IBatteryStats;
+import com.android.internal.os.BatteryStatsImpl;
+
+import android.os.BatteryStats;
+import android.util.Log;
+
+import android.os.MemoryFile;
+import android.os.Parcel;
+import android.os.ParcelFileDescriptor;
+
+import java.io.FileInputStream;
+
+import android.os.SystemClock;
 
 /**
  * Support for storage of key-value pairs which persists across device wipes /
@@ -48,43 +60,85 @@ public class DeviceBattery {
 
     private static IBatteryStats mBatteryStats;
 
-    static{
+    static {
         mBatteryStats = BatteryStatsService.getService();
     }
 
-    public static long getRemainingBatteryTime(){
+    public static long getRemainingBatteryTime() {
         try {
             return mBatteryStats.computeBatteryTimeRemaining();
-        }
-        catch(RemoteException e){
+        } catch (RemoteException e) {
             return -101;
         }
     }
 
-    public static long getChargeRemainingTime(){
+    public static long getChargeRemainingTime() {
         try {
             return mBatteryStats.computeChargeTimeRemaining();
-        }
-        catch(RemoteException e){
+        } catch (RemoteException e) {
             return -101;
         }
     }
 
-    public static boolean isCharging(){
+    public static boolean isCharging() {
         try {
             return mBatteryStats.isCharging();
-        }
-        catch(RemoteException e){
+        } catch (RemoteException e) {
             return false;
         }
     }
 
-    public static long getAwakeTime(){
+    public static long getScreenOnTime() {
+        BatteryStats batteryStats = getStats(mBatteryStats);
+        return batteryStats.getScreenOnTime(SystemClock.elapsedRealtime() * 1000,
+                BatteryStats.STATS_SINCE_CHARGED) / 1000;
+    }
+
+    private static BatteryStatsImpl getStats(IBatteryStats service) {
         try {
-            return mBatteryStats.getAwakeTimeBattery();
+            ParcelFileDescriptor pfd = service.getStatisticsStream();
+            if (pfd != null) {
+                try (FileInputStream fis = new ParcelFileDescriptor.AutoCloseInputStream(pfd)) {
+                    byte[] data = readFully(fis, MemoryFile.getSize(pfd.getFileDescriptor()));
+                    Parcel parcel = Parcel.obtain();
+                    parcel.unmarshall(data, 0, data.length);
+                    parcel.setDataPosition(0);
+                    BatteryStatsImpl stats = com.android.internal.os.BatteryStatsImpl.CREATOR
+                            .createFromParcel(parcel);
+                    return stats;
+                } catch (IOException e) {
+                    Log.w(TAG, "Unable to read statistics stream", e);
+                }
+            }
+        } catch (RemoteException e) {
+            Log.w(TAG, "RemoteException:", e);
         }
-        catch(RemoteException e){
-            return -101;
+        return new BatteryStatsImpl();
+    }
+
+    public static byte[] readFully(FileInputStream stream) throws java.io.IOException {
+        return readFully(stream, stream.available());
+    }
+
+    public static byte[] readFully(FileInputStream stream, int avail) throws java.io.IOException {
+        int pos = 0;
+        byte[] data = new byte[avail];
+        while (true) {
+            int amt = stream.read(data, pos, data.length - pos);
+            //Log.i("foo", "Read " + amt + " bytes at " + pos
+            //        + " of avail " + data.length);
+            if (amt <= 0) {
+                //Log.i("foo", "**** FINISHED READING: pos=" + pos
+                //        + " len=" + data.length);
+                return data;
+            }
+            pos += amt;
+            avail = stream.available();
+            if (avail > data.length - pos) {
+                byte[] newData = new byte[pos + avail];
+                System.arraycopy(data, 0, newData, 0, pos);
+                data = newData;
+            }
         }
     }
 
